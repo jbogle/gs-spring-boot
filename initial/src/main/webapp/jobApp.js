5395cf94b5a5e566030107c6b96fc0b5f5770419
Janet Bogle <janet.bogle@Janet-Bogles-MacBook-Pro.local>
2017-07-19 09:59:02 -0400

    first cut at front end (not hooked up to back yet)

---
+++ initial/src/main/webapp/jobApp.js
@@ -0,0 +1,73 @@
+/**
+ * Created by janet.bogle on 7/17/17.
+ */
+// create drop down with choices
+const showDropDown = (divStr, elementStr, category) => {
+    console.log("In showDropDown");
+    showArea(divStr);
+    let element = document.getElementsByClassName(elementStr);
+    // get choices from db. Mocked for now.
+    let jobChoices = getJobFieldChoices(category);
+    // load the drop-down on the form with choices
+    loadChoices(element[0], jobChoices);
+}
+
+// make the areas visible when we come to them
+const showArea = (divStr) => {
+    "use strict";
+    let element = document.getElementsByClassName(divStr);
+    // we set default to hidden. See if default is still on
+    if (divStr === "main" && getComputedStyle(element[0]).visibility === "hidden") {
+        // Show first drop down on startup
+        element[0].style.visibility = "visible";
+    }
+    else if (divStr === "sub" && getComputedStyle(element[0]).visibility === "hidden") {
+        // Show sub-Field drop down after 1st category is chosen
+        element[0].style.visibility = "visible";
+    }
+}
+
+// add the option attributes to the select element
+const loadChoices = (element, choices) => {
+    // first clear the drop down list
+    element.options.length = 0;   // why does it think this is a constant??
+    element.size = choices.length;      // set drop down size to show all elements
+    // for each choice, add an option attribute to the select tag
+    choices.forEach(function(option) {
+        element.options.add(new Option(option, option));
+    });
+}
+
+// get applicable choices
+function getJobFieldChoices(category) {
+    // query db for choices, mock for now
+    switch (category) {
+        case 'mainChoices':  // top-level drop down
+            return ["energy", "food", "pharmaceuticals", "miscellaneous"];
+            break;
+
+        case 'energy':       // get choices for energy category
+            return ["traditional", "renewable"];
+            break;
+
+        case 'food':       // get choices for food category
+            return ["no further choices"];
+            break;
+
+        case 'pharmaceuticals':       // get choices for pharma category
+            return ["no further choices"];
+            break;
+
+        case 'miscellaneous':        // get choices for misc category
+            return ["paint", "plastics", "fertilizer", "construction materials"];
+            break;
+
+        default:
+            break;
+    }
+}
+// initialize with first drop-down
+(function appInit() {
+    showDropDown("main", "jobField", "mainChoices");
+    console.log("Inside the app init");
+})();
