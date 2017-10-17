127cca4b8afaa94749533803c932fa643369af0a
jbogle <janet.bogle@cantina.co>
2017-10-17 15:53:10 -0400

    data (hard-coded) is sent to react f/e and displayed

--- initial/src/main/java/hello/AppController.java
+++ initial/src/main/java/hello/AppController.java
@@ -2,76 +2,70 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * Created by janet.bogle on 7/21/17.
  */
-//@Controller
-@RestController
+@Controller
+//@RestController
 public class AppController {
 
     @Autowired
     public ChemERepository chemERepository;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getJobChoices")
-    public ModelAndView getJobChoices() {
-
-        //ModelMap model = new ModelMap();
-
-        //List<String> jobs;
-        //jobs = chemERepository.fetchJobs();
-
-        ChemEJobForm form = new ChemEJobForm();
-        form.setMainfield("got it");
-        //model.addAttribute("form", form);
-        //model.addAttribute("jobs", jobs);
-        // model.addAttribute("tryIt", "Got here");
-
-        //ModelAndView mv = new ModelAndView("", model);
-
-        return new ModelAndView("/jobChoices.html", "chemejobform", form);
-        //return "form";
-    }
-
-    @RequestMapping(method = RequestMethod.POST, value = "/saveJobChoice")
-    //@ResponseBody
-    public void saveJobChoice(@ModelAttribute("chemEJobForm") ChemEJobForm jobForm
-                              //javax.servlet.http.HttpServletRequest request
-    ) {
-        //return "in nowWhat";  // to return just data to the browser: use @ResponseBody on the method and return String
-
-        //System.out.println(jobForm.getMainField());
-        //System.out.println(jobForm.getSubField());
-
-
-    }
-
+   // this works when using @RestController vs @Controller
     @RequestMapping("/endpoint")
     public String endpoint() {
         return "in AppController.endpoint\n";
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/getOneJob")
-    public ModelAndView getOneJob() {
 
-        //ModelMap model = new ModelMap();
-        //List<String> jobs;
-        //jobs = chemERepository.fetchJobs();
+
+/*  Using this with React front end, this method gets called, but 404 is returned.
+    Suspect the 'view' isn't correct. Look at View object, my ViewResolver, etc.
+    Not even sure I want a 'view' though, so.....
+
+    @RequestMapping(method = RequestMethod.GET, value = "/fetchJobs")
+    public ModelAndView fetchJobs() {
+        System.out.println("in /fetchJobs");
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("JobsData.js");
 
         ChemEJobForm form = new ChemEJobForm();
         form.setMainfield("got it");
-        //model.addAttribute("form", form);
-        //model.addAttribute("jobs", jobs);
-        // model.addAttribute("tryIt", "Got here");
 
-        //ModelAndView mv = new ModelAndView("", model);
+        modelAndView.addObject("form", form);
 
-        return new ModelAndView("/jobFind.js", "chemejobform", form);
-        //return "form";
+        return modelAndView;
     }
+    */
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "/fetchJobs")
+    public List<String> fetchJobs() {
+        System.out.println("in /fetchJobs");
+
+        List<String> jobs = new ArrayList<>();
+        jobs.add("try");
+        jobs.add("this");
+
+        return jobs;
+    }
+
 }
+
+
+
+
+
+
+
+
